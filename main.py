8b985e81439016d9279566c3b6a7b28f54fce22e
Houjo <lolikion22829@gmail.com>
2023-01-06 15:15:31 +0300

    12

---
+++ main.py
@@ -0,0 +1,16 @@
+
+from turtle import *
+tracer(0)
+
+def hi():
+    for i in range(10):
+        fd(100)
+        rt(60)
+    update()
+    exitonclick()
+
+
+if __name__ == '__main__':
+    hi()
+
+
