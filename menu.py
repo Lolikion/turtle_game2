76e24dfdcbf3c9f98e29dd5f703f2894c9862650
Houjo <lolikion22829@gmail.com>
2023-01-19 22:03:04 +0300

    12

--- menu.py
+++ menu.py
@@ -8,13 +8,17 @@ from time import strftime
 from tkinter import *
 from tkinter import ttk
 
+
+game_state=0
+pause_state=0
 inv=1 #num of standart invertory cell
 hp=19
 table_state=0
 
 
 def open_game():
-    global menu_window,menu_frm,h
+    global menu_window,menu_frm,h,game_state
+    game_state=1
     menu_frm.grid_forget()
     game_screen.grid()
     game_screen.create_image(0, 0, image=hp_bar, anchor=NW)
@@ -30,14 +34,16 @@ def change_time():
 
 
 def open_oz():
-    global oz_obj
-    oz_obj = game_screen.create_image(0, 0, image=oz1, anchor=NW)
-    tablet_time.place_forget()
-    return oz_obj
+    global oz_obj,pause_state
+    if pause_state==0:
+        oz_obj = game_screen.create_image(0, 0, image=oz1, anchor=NW)
+        tablet_time.place_forget()
+        return oz_obj
 
 
 def change_tablet():
     global table_state,game_screen,tablet_time
+
     if table_state==0:
         open_oz()
         table_state = 1
@@ -48,16 +54,21 @@ def change_tablet():
 
 
 def pause():
-    global pause_frm
-    pause_frm.grid()
-    print(228)
+    global pause_frm,game_state,pause_state,game_screen,h,w
+    if game_state==1:
+        if pause_state==0:
+            pause_frm.place(x=w//2-120,y=h//2-160)
+            pause_state=1
+        elif pause_state==1:
+            pause_frm.place_forget()
+            pause_state=0
 
 def key_press(event):
-    global inv,invertory,menu_window
-    if event.keysym in '1234':
+    global inv,invertory,menu_window,pause_state
+    if event.keysym in '1234' and pause_state==0:
         inv=int(event.keysym)
         invertory.configure(file = f"C://Users//Houjo//PycharmProjects//turtle_game//pics//invertory//inv{inv}.png")
-    elif event.keysym=='space':
+    elif event.keysym=='space' and pause_state==0:
         change_tablet()
     elif event.keysym=='Escape':
         pause()
@@ -68,24 +79,33 @@ def open_menu_from_settings():
     settings_frm.grid_forget()
     menu_frm.grid()
 
+def open_menu_from_pause():
+    global game_screen, menu_frm, pause_frm
+    pause_frm.place_forget()
+    game_screen.grid_forget()
+    menu_frm.grid()
 
 def change_language(event):
     global language_change
     cur=language_change.get()
     if cur == 'Русский':
         menu_button.configure(text='Назад в меню')
+        menu_button2.configure(text='Назад в меню')
         exit_button.configure(text='Выход')
         start_button.configure(text='Начать игру')
         settings_button.configure(text='Настройки')
         settings_button2.configure(text='Настройки')
         controls_button.configure(text='Управление')
+        pause_lbl.configure(text='Игра на паузе,чел)))')
         language_change.current(1)
     if cur == 'English':
         menu_button.configure(text='Back to menu')
+        menu_button2.configure(text='Back to menu')
         exit_button.configure(text='Exit')
         start_button.configure(text='Start game')
         settings_button.configure(text='Settings')
         settings_button2.configure(text='Settings')
+        pause_lbl.configure(text='Game is paused, man)))')
         controls_button.configure(text='Controls')
         language_change.current(0)
 
@@ -118,15 +138,16 @@ def open_controls():
 # All widgets
 menu_window = Tk()
 w, h = menu_window.winfo_screenwidth(), menu_window.winfo_screenheight()
-print(w,h)
+
+game_screen = Canvas(menu_window,width = w, height = h, bg = 'blue')
 menu_frm = ttk.Frame(menu_window,width=w,height=h)
 settings_frm = ttk.Frame(menu_window, width=w, height=h)
 controls_frm = ttk.Frame(menu_window, width=w, height=h)
-
-
-
+pause_frm = ttk.Frame(game_screen, width=200, height=240)
 
 menu_button = Button(settings_frm, text='Back to menu', command=open_menu_from_settings)
+menu_button2 = Button(pause_frm, text='Back to menu', command=open_menu_from_pause)
+menu_button2.place(relx=0.3,rely=0.8)
 start_button=Button(menu_frm,text='Start game',command=open_game)
 settings_button=Button(menu_frm,text='Settings',command=open_settings_from_menu)
 settings_button2=Button(controls_frm,text='Settings',command=open_settings_from_controls)
@@ -134,7 +155,7 @@ language_change = ttk.Combobox(settings_frm,values=['English','Русский'])
 exit_button=Button(menu_frm,text='Exit',command=menu_window.destroy)
 controls_button=Button(settings_frm,text='Controls',command=open_controls)
 
-game_screen = Canvas(menu_window,width = w, height = h, bg = 'blue')
+
 game_bg=PhotoImage(file = "C://Users//Houjo//PycharmProjects//turtle_game//pics//backgrounds//bg1.png")
 hp_bar=PhotoImage(file = f"C://Users//Houjo//PycharmProjects//turtle_game//pics//hpbar//hpbar_{hp}.png")
 invertory=PhotoImage(file = f"C://Users//Houjo//PycharmProjects//turtle_game//pics//invertory//inv{inv}.png")
@@ -142,8 +163,10 @@ tablet_closed=PhotoImage(file = f"C://Users//Houjo//PycharmProjects//turtle_game
 oz1=PhotoImage(file = f"C://Users//Houjo//PycharmProjects//turtle_game//pics//tablet//tablet_opened1.png")
 
 tablet_time=ttk.Label(game_screen)
-pause_frm = ttk.Frame(game_screen, width=200, height=200)
-pause_lbl = ttk.Label(pause_frm,text='lol')
+pause_lbl=ttk.Label(pause_frm,text='Game is paused, man)))')
+pause_lbl.place(relx=0.2,rely=0.2)
+
+
 
 # Use this to configure widgets
 menu_window.attributes("-fullscreen", True)
@@ -155,6 +178,7 @@ language_change.current(0)
 tablet_time.configure(text=strftime("%H:%M:%S:%p"),font=(30))
 game_screen.create_image(0, 0, image = game_bg, anchor = NW)
 menu_window.bind("<KeyPress>",key_press)
+
 
 menu_window.mainloop()
 
